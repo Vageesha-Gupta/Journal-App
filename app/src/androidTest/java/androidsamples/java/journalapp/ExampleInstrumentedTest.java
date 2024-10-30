32101d01503ac3d9da1d867b8546783c7d499557
vageesha_gupta <f20221107@goa.bits-pilani.ac.in>
2024-10-30 18:11:03 +0530

    test cases for task1 and task2.

--- app/src/androidTest/java/androidsamples/java/journalapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/androidsamples/java/journalapp/ExampleInstrumentedTest.java
@@ -1,13 +1,17 @@
 package androidsamples.java.journalapp;
 
+import org.hamcrest.Matchers;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import androidx.test.espresso.contrib.PickerActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
+
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 
 
@@ -15,7 +19,10 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import android.widget.DatePicker;
 
 import androidx.test.espresso.accessibility.AccessibilityChecks;
 
@@ -37,6 +44,56 @@ public class ExampleInstrumentedTest {
 
     //
     @Test
+    public void testNavigateToCreateEntry() {
+        // Click on the "Create Entry" button
+        onView(withId(R.id.btn_add_entry)).perform(click());
+
+        // Check if the "Create Entry" screen is displayed by verifying if specific fields are visible
+        onView(withId(R.id.edit_title)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_entry_date)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_start_time)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_end_time)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testEnterEntryDetailsAndSave() {
+        // Navigate to "Create Entry" screen
+        onView(withId(R.id.btn_add_entry)).perform(click());
+
+        // Enter title
+        onView(withId(R.id.edit_title)).perform(replaceText("My Test Journal Entry"));
+
+        // Select date, start time, and end time (assuming date and time pickers are correctly set up)
+        onView(withId(R.id.btn_entry_date)).perform(click());
+        //  date picker interaction
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName())))
+                .perform(PickerActions.setDate(2024, 10, 28)); // Replace with desired year, month, day
+        onView(withId(android.R.id.button1)).perform(click());
+        onView(withId(R.id.btn_start_time)).perform(click());
+        // Add start time picker interaction here if needed
+
+        onView(withId(R.id.btn_end_time)).perform(click());
+        // Add end time picker interaction here if needed
+
+        // Click "Save Entry" button to save
+        onView(withId(R.id.btn_save)).perform(click());
+
+        // Verify the Entry List screen or confirmation message is displayed
+        onView(withId(R.id.recyclerView)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testBackNavigationFromCreateEntry() {
+        // Navigate to "Create Entry" screen
+        onView(withId(R.id.btn_add_entry)).perform(click());
+
+        // Press back button to return to the main screen
+        pressBack();
+
+        // Verify main screen (Entry List) is displayed again
+        onView(withId(R.id.recyclerView)).check(matches(isDisplayed()));
+    }
+    @Test
     public void testNavigationToEntryDetails() {
         // Click on the add entry button in EntryListFragment
         onView(withId(R.id.btn_add_entry)).perform(click());
@@ -56,6 +113,7 @@ public class ExampleInstrumentedTest {
         // Verify that EntryListFragment is displayed again by checking for a unique view in EntryListFragment
         onView(withId(R.id.recyclerView)).check(matches(isDisplayed())); // RecyclerView ID in EntryListFragment
     }
+
     //over
 
 
